e7198c519775a50a598bb45bd96d7ac530d4868c
Joseph Rogers <RogersJoseph@JohnDeere.com>
2020-10-09 11:27:42 -0500

    Buttons given functions and start of logic

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 //console.log("connected");
 
 //global variables
-const start = document.getElementById("start");
+const startButton = document.getElementById("start");
 const playAgain = document.getElementById("playAgain");
-const green = document.getElementById("green");
+const greenButton = document.getElementById("green");
+const redButton = document.getElementById("red");
+const yellowButton = document.getElementById("yellow");
+const blueButton = document.getElementById("blue");
 let compPattern = [];
 let userPattern = [];
+let newnumber;
 
-//Start Button Logic
-start.addEventListener("click", (event) => {
-    event.preventDefault();
-    game();
-});
+//Buttons
+startButton.addEventListener("click", start());
+greenButton.addEventListener("click", green());
+redButton.addEventListener("click", red());
+yellowButton.addEventListener("click", yellow());
+blueButton.addEventListener("click", blue());
 
 
-
-
-//Game Logic
-function game(){
+//Start Logic
+function start(){
+    preventDefault();
     playback();
-    test();
-    loser();
-};
-
-
-
+}
 
 
 //Select Playback Function
 function playback(){
-
+    //add random number to array
+    let compNumber = Math.floor(Math.random()*3);
+    compPattern.push(compNumber);
+    console.log(compPattern)
+    //show pattern on screen
 };
 
 
@@ -37,10 +40,15 @@ function playback(){
 
 //Select User Test Function
 function test(){
-    green();
-    red();
-    yellow();
-    blue();
+    let newSpace = userPattern.length - 1;
+    if(userPattern.length === compPattern.length){
+        userPattern = [];
+        game();
+    } else if(userPattern[newSpace] != compPattern[newSpace]){
+        return;
+    } else {
+        loser;
+    }
 };
 
 
@@ -48,16 +56,28 @@ function test(){
 
 //funcion for individual color presses
 function green(){
-
+    newNumber = 0;
+    userPattern.push(newNumber)
+    console.log("Green")
+    test();
 };
 function red(){
-
+    newNumber = 1;
+    userPattern.push(newNumber)
+    console.log("Red")
+    test();
 };
 function yellow(){
-
+    newNumber = 2;
+    userPattern.push(newNumber)
+    console.log("Yellow")
+    test();
 };
 function blue(){
-
+    newNumber = 3;
+    userPattern.push(newNumber)
+    console.log("Blue")
+    test();
 };
 
 
@@ -65,7 +85,7 @@ function blue(){
 
 //Loser Screen Function
 function loser(){
-
+    console.log("loser");
 };
 //Select Playback Function
     //Select User Test Function

93584d11fb8f1217a21b2747bf49518bd4979b38
Joseph Rogers <RogersJoseph@JohnDeere.com>
2020-10-12 11:22:53 -0500

    Loser code is complete and code is started to clean

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //console.log("connected");
 
 //global variables
+
 const startButton = document.getElementById("start");
-const playAgain = document.getElementById("playAgain");
+// const playAgain = document.getElementById("playAgain");
+const buttons = document.getElementsByClassName("button");
 const greenButton = document.getElementById("green");
 const redButton = document.getElementById("red");
 const yellowButton = document.getElementById("yellow");
@@ -10,7 +12,20 @@ const blueButton = document.getElementById("blue");
 let compPattern = [];
 let userPattern = [];
 let newnumber;
+let move = 1000;
+let light = move*.75;
 
+//global functions
+function green(){greenButton.style.backgroundColor = "lightgreen";}
+function red(){redButton.style.backgroundColor = "purple";}
+function yellow(){yellowButton.style.backgroundColor = "purple";}
+function blue(){blueButton.style.backgroundColor = "lightblue";}
+function originalColors(){
+    greenButton.style.backgroundColor = "";
+    redButton.style.backgroundColor = "";
+    yellowButton.style.backgroundColor = "";
+    blueButton.style.backgroundColor = "";
+}
 
 //Buttons
 startButton.addEventListener("click", start);
@@ -39,9 +54,12 @@ blueButton.addEventListener("click", () =>{
 //Start Logic
 function start(){
     //event.preventDefault();
+    document.getElementById("green").disabled = false;
+    document.getElementById("red").disabled = false;
+    document.getElementById("yellow").disabled = false;
+    document.getElementById("blue").disabled = false;
     playback();
 }
-
 
 //Select Playback Function
 function playback(){
@@ -57,34 +75,24 @@ function playback(){
         if(counter === compPattern.length) {
             clearInterval(a);
         }
-    }, 1000);
+    }, move);
 };
-
 
 //showing computer pattern with color changes
 async function showPattern(show) {
     console.log(show);
     if(show == 0){
-        greenButton.style.backgroundColor = "lightgreen";
-        setTimeout(() => {
-            greenButton.style.backgroundColor = "";   
-         }, 750);
+        green();
     } else if (show == 1){
-        redButton.style.backgroundColor = "purple";
-        setTimeout(() => {
-            redButton.style.backgroundColor = "";
-        }, 750); 
+        red();
     } else if (show == 2) {
-        yellowButton.style.backgroundColor = "purple";
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "";
-        }, 750);
+        yellow();
     } else if (show == 3) {
-        blueButton.style.backgroundColor = "lightblue";
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "";
-        }, 750);
+        blue();
     }
+    setTimeout(() => {
+        originalColors();
+     }, light);
 };
 
 
@@ -100,97 +108,46 @@ function test(){
         return;
     } else {
         loser();
+        // compPattern = [];
+        // userPattern = [];
     }
 };
-
-
 
 //Loser Screen Function
 function loser(){
     let counter = 0;
     let a = setInterval(function(){
         loserColor(compPattern);
+        setTimeout(() => {
+            originalColors(); 
+         }, 200); 
         counter++;
         if(counter === 4) {
             clearInterval(a);
         }
     }, 250);
-    console.log("loser");
-    greenButton.disable = "true";
+    setTimeout(() => {
+        originalColors();
+        compPattern = [];
+        userPattern = [];
+    }, 1000); 
+    document.getElementById("green").disabled = true;
+    document.getElementById("red").disabled = true;
+    document.getElementById("yellow").disabled = true;
+    document.getElementById("blue").disabled = true;
 };
 
 function loserColor() {
-    console.log(userPattern);
     let wrongNumber = (compPattern[userPattern.length-1]);
     if(wrongNumber == 0){
-        greenButton.style.backgroundColor = "lightgreen";
-        setTimeout(() => {
-           greenButton.style.backgroundColor = "";   
-        }, 450); 
-        setTimeout(() => {
-            greenButton.style.backgroundColor = "lightgreen";   
-        }, 450); 
-        setTimeout(() => {
-        greenButton.style.backgroundColor = "";   
-        }, 450); 
-        setTimeout(() => {
-            greenButton.style.backgroundColor = "lightgreen";   
-        }, 450); 
-        setTimeout(() => {
-        greenButton.style.backgroundColor = "";   
-        }, 450); 
-        
+        green();
     } else if (wrongNumber == 1){
-        redButton.style.backgroundColor = "purple";
-        setTimeout(() => {
-            redButton.style.backgroundColor = "";
-        }, 250); 
-        setTimeout(() => {
-            redButton.style.backgroundColor = "purple";
-        }, 250);
-        setTimeout(() => {
-            redButton.style.backgroundColor = "";
-        }, 250); 
-        setTimeout(() => {
-            redButton.style.backgroundColor = "purple";
-        }, 250);
-        setTimeout(() => {
-            redButton.style.backgroundColor = "";
-        }, 250); 
+        red();
     } else if (wrongNumber == 2) {
-        yellowButton.style.backgroundColor = "purple";
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "";
-        }, 250);
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "purple";
-        }, 250);
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "";
-        }, 250);
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "purple";
-        }, 250);
-        setTimeout(() => {
-            yellowButton.style.backgroundColor = "";
-        }, 250);
+        yellow();
     } else if (wrongNumber == 3) {
-        blueButton.style.backgroundColor = "lightblue";
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "";
-        }, 250); 
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "lightblue";
-        }, 250); 
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "";
-        }, 250); 
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "lightblue";
-        }, 250); 
-        setTimeout(() => {
-            blueButton.style.backgroundColor = "";
-        }, 250); 
+        blue();
     }
 };
+
 
